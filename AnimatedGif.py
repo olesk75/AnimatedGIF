40c11f22a2d5e3b1b45f933402f4e11a10a7297b
olesk75 <olesk@pvv.org>
2016-02-09 10:12:13 +0100

    Code cleanup, made python 2.x compatible (untested)

--- AnimatedGif.py
+++ AnimatedGif.py
@@ -7,25 +7,29 @@ Released under the terms of the GNU General Public License, version 3
 
 """
 from threading import Thread
-import tkinter as tk
 import time
+try:
+	# for Python2
+	import Tkinter as tk
+except ImportError:
+	# for Python3
+	import tkinter as tk
 
 
 class AnimatedGif(tk.Label):
 	"""
 	Class to show animated GIF file in a label
-	:root: Tk parent
-	:gif_file: path and filename of animated gif
-	:delay: delay in seconds (or parts of seconds) between each frame
-
 	Use start() method to begin animation, and set the stop flag to stop it
 	"""
 	def __init__(self, root, gif_file, delay=0.04):
+		"""
+		:param root: tk.parent
+		:param gif_file: filename (and path) of animated gif
+		:param delay: delay between frames in the gif animation (float)
+		"""
 		tk.Label.__init__(self, root)
-		self.root = root
 		self.gif_file = gif_file
 		self.delay = delay  # Animation delay - try low floats, like 0.04
-		self.gif = tk.PhotoImage(file=gif_file)
 
 		self._num = 0
 		self.animation_thread = Thread()
@@ -40,8 +44,6 @@ class AnimatedGif(tk.Label):
 				time.sleep(self.delay)
 				self.gif = tk.PhotoImage(file=self.gif_file, format='gif -index {}'.format(self._num))
 				self.configure(image=self.gif)
-				#self.image = self.gif  # We anchor image object to avoid grabage collection removing it
-
 				self._num += 1
 			except tk.TclError:
 				self._num = 0
